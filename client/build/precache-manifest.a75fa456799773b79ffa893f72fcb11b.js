13e9514e4a661098b7a5d12720dbf865c8120d16
YvelStLaurent <rachaelbelle@gmail.com>
2019-07-09 13:20:06 -0400

    initial commit

---
+++ client/build/precache-manifest.a75fa456799773b79ffa893f72fcb11b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "495d93cc2c05774c4e88882137125113",
+    "url": "/static/media/logo.495d93cc.svg"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "1a4f3bc8c4fb293502aa",
+    "url": "/static/js/main.020b25c9.chunk.js"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "1a4f3bc8c4fb293502aa",
+    "url": "/static/css/main.a6c98faf.chunk.css"
+  },
+  {
+    "revision": "a750e21be3ceca565e8a79c4eedb0e08",
+    "url": "/index.html"
+  }
+];
